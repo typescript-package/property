469238f2c3cf8a36d490c516d40227b3fbf3c008
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-07-03 00:02:21 +0000

    refactor(`WrappedPropertyDescriptor`): use `AccessorPropertyDescriptor`.

--- src/wrap/interface/wrapped-property-descriptor.interface.ts
+++ src/wrap/interface/wrapped-property-descriptor.interface.ts
@@ -1,10 +1,12 @@
-import { GetterCallback, SetterCallback } from "@typedly/callback";
+import { AccessorPropertyDescriptor } from '@typedly/descriptor';
+import { GetterCallback, SetterCallback } from '@typedly/callback';
 
 export interface WrappedPropertyDescriptor<
   O extends object,
   K extends keyof O
-> extends Pick<PropertyDescriptor, 'configurable' | 'enumerable'> {
-  onGet?: GetterCallback<O, K>,
-  onSet?: SetterCallback<O, K>,
-  privateKey?: PropertyKey,
+> extends AccessorPropertyDescriptor<O[K]> {
+  active?: boolean;
+  privateKey?: PropertyKey;
+  onGet?: GetterCallback<O, K>;
+  onSet?: SetterCallback<O, K>;
 }
