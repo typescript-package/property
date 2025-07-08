7052740b249743d7675c7412043c3ea88a2819c7
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-07-08 15:19:46 +0000

    refactor(`WrappedPropertyDescriptor`): add generic type variable for `active` and make it union to determine `active` state of specific callback `onGet` or `onSet`

--- src/wrap/interface/wrapped-property-descriptor.interface.ts
+++ src/wrap/interface/wrapped-property-descriptor.interface.ts
@@ -4,11 +4,31 @@ import { GetterCallback, SetterCallback } from '@typedly/callback';
 export interface WrappedPropertyDescriptor<
   O,
   K extends keyof O,
+  A extends boolean = boolean,
   C extends boolean = boolean,
   E extends boolean = boolean,
 > extends AccessorPropertyDescriptor<O[K], C, E> {
-  active?: boolean;
+  /**
+   * @description Whether the property descriptor `onGet` and `onSet` callbacks are active.
+    * @type {?(A | {onGet?: boolean; onSet?: boolean})}
+   */
+  active?: A | {onGet?: boolean; onSet?: boolean};
+
+  /**
+   * @description The key used to access the property in the object.
+   * @type {?PropertyKey}
+   */
   privateKey?: PropertyKey;
+
+  /**
+   * @description The callback function that is called when the property is accessed.
+   * @type {?GetterCallback<O, K>}
+   */
   onGet?: GetterCallback<O, K>;
+
+  /**
+   * @description The callback function that is called when the property is set.
+   * @type {?SetterCallback<O, K>}
+   */
   onSet?: SetterCallback<O, K>;
 }
